4807317adff8e1a6a19a005d3bf34e3834d69696
Firagos <jemalfiragos@gmail.com>
2025-05-12 11:20:48 +0300

    i modify it

--- Server/model/user.js
+++ Server/model/user.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, "Please provide a name of the product"],
+    required: [true, "Please provide a name"],
   },
   password: {
     type: String,
@@ -12,7 +13,19 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, "Please provide an email"],
+    unique: true,
+    lowercase: true,
   },
+});
+
+// Hash the password before saving
+userSchema.pre("save", async function (next) {
+  // Only hash if password is new or modified
+  if (!this.isModified("password")) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
