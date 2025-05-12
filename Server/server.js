04afebe8af60a15b0841474bb0d5d93c66df500b
Firagos <jemalfiragos@gmail.com>
2025-05-12 22:15:34 +0300

    i modify it

--- Server/server.js
+++ Server/server.js
@@ -4,8 +4,7 @@ import dotenv from "dotenv";
 import productRoute from "./route/product.js";
 import userRoute from "./route/user.js";
 import cartRouter from "./route/cart.js";
-import orderRouter from "./routes/order.js";
-
+import orderRouter from "./route/order.js";
 // Load environment variables
 dotenv.config();
 
@@ -25,7 +24,7 @@ mongoose
 app.use("/api/v3/product", productRoute);
 app.use("/api/v3/user", userRoute);
 app.use("/api/v3/cart", cartRouter);
-app.use("/api/orders", orderRouter);
+app.use("/api/v3/order", orderRouter);
 
 const port = process.env.PORT || 4000;
 
