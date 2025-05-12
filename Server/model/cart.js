3823eb0a31541bdfeba5203abf1302611008cc85
Firagos <jemalfiragos@gmail.com>
2025-05-12 11:31:49 +0300

    i modify it

--- Server/model/cart.js
+++ Server/model/cart.js
@@ -1,23 +1,17 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
+
 const cartItemSchema = new mongoose.Schema({
   product: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Product",
     required: true,
   },
-  quantity: {
-    type: Number,
-    default: 1,
-  },
+  variantColor: { type: String, required: true },
+  quantity: { type: Number, default: 1 },
 });
 
 const cartSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-    unique: true,
-  },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   items: [cartItemSchema],
 });
 
