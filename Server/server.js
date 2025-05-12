acb36cd22a24f599377b3c9983cc57c5c24e67be
Firagos <jemalfiragos@gmail.com>
2025-05-12 08:39:40 +0300

    i modify it

--- Server/server.js
+++ Server/server.js
@@ -1,10 +1,27 @@
 import express from "express";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
-
+import productRoute from "./route/product.js";
+import userRoute from "./route/user.js";
 // Load environment variables
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => {
+    console.error("❌ MongoDB connection error:", err);
+    process.exit(1); // Exit if DB connection fails
+  });
+
+app.use("/api/v3/product", productRoute);
+app.use("/api/v3/user", userRoute);
+
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
