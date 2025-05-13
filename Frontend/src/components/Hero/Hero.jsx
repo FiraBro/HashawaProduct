93e68124b321974eaaa6ffb964d6fde51ae9ace5
Firagos <jemalfiragos@gmail.com>
2025-05-13 22:24:43 +0300

    i create her section

---
+++ Frontend/src/components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from '../Hero/Hero.module.css';
+
+const Hero = () => (
+  <section className={styles.hero} id="home">
+    <div className={styles.heroContent}>
+      <div
+        className={styles.contentBg}
+        style={{ backgroundImage: "url('shoe.jpg')" }}
+      ></div>
+      <div className={styles.contentText}>
+        <h1>Welcome to Abdihope Shoe Store</h1>
+        <p>Your one-stop shop for the latest shoe trends!</p>
+        <a href="#products" className={styles.btn}>Shop Now</a>
+      </div>
+    </div>
+  </section>
+);
+
+export default Hero;
