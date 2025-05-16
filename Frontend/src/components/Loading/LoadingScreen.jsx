bdfa31af085c95e936da294226394609a73ceaef
Firagos <jemalfiragos@gmail.com>
2025-05-16 23:38:46 +0300

    i create new loading spinner

---
+++ Frontend/src/components/Loading/LoadingScreen.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./LoadingScreen.module.css";
+
+export default function LoadingScreen() {
+  return (
+    <div className={styles.overlay}>
+      <div className={styles.loaderWrapper}>
+        <div className={styles.spinner}></div>
+        <h1 className={styles.brandText}>AbdiHope</h1>
+      </div>
+    </div>
+  );
+}
