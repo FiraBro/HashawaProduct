06930fdcdc3cc723b9f56f928d8b9762946c9571
Firagos <jemalfiragos@gmail.com>
2025-05-16 17:37:26 +0300

    i finally finish it

--- Frontend/src/components/Testimonial/Testimonial.jsx
+++ Frontend/src/components/Testimonial/Testimonial.jsx
@@ -1,16 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { reviewService } from '../../Service/reviewService';
-import styles from '../Testimonial/Testimonial.module.css';
+import React, { useEffect, useState } from "react";
+import { reviewService } from "../../Service/reviewService";
+import styles from "../Testimonial/Testimonial.module.css";
 
 const Testimonials = () => {
   const [testimonials, setTestimonials] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  const BACKEND_URL = "http://localhost:3000";
   useEffect(() => {
     const fetchTestimonials = async () => {
       try {
         const data = await reviewService.getTestimonials();
+        console.log(data);
         setTestimonials(data);
       } catch (err) {
         setError(err.message);
@@ -32,15 +34,21 @@ const Testimonials = () => {
         {testimonials.map((testimonial) => (
           <div key={testimonial._id} className={styles.card}>
             <div className={styles.header}>
-              <img 
-                src={testimonial.userId.avatar || '/images/default-avatar.png'} 
+              <img
+                src={
+                  testimonial.userId.userImage
+                    ? `${BACKEND_URL}/uploads/userImage/${testimonial.userId.userImage}`
+                    : "/images/default-avatar.png"
+                }
                 alt={testimonial.userId.name}
                 className={styles.avatar}
               />
+
               <div>
                 <h4>{testimonial.userId.name}</h4>
                 <div className={styles.rating}>
-                  {'★'.repeat(testimonial.rating)}{'☆'.repeat(5 - testimonial.rating)}
+                  {"★".repeat(testimonial.rating)}
+                  {"☆".repeat(5 - testimonial.rating)}
                 </div>
               </div>
             </div>
@@ -48,10 +56,6 @@ const Testimonials = () => {
             <p>{testimonial.comment}</p>
             <div className={styles.product}>
               <span>Purchased:</span>
-              <img 
-                src={testimonial.productId.mainImage} 
-                alt={testimonial.productId.name}
-              />
               <p>{testimonial.productId.name}</p>
             </div>
           </div>
