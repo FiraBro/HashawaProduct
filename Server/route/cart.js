d66f725d903b7f4820c37fe27beb6bd68822edba
Firagos <jemalfiragos@gmail.com>
2025-05-16 22:32:44 +0300

    i add uodate cart function to it

--- Server/route/cart.js
+++ Server/route/cart.js
@@ -1,13 +1,21 @@
 import express from "express";
 const cartRouter = express.Router();
+
 import {
   addToCart,
   removeFromCart,
-  getCart
+  getCart,
+  updateCartQuantity, // 👈 Import the new controller function
 } from "../controller/cartController.js";
+
 import { Protect } from "../controller/authController.js";
+
+// Existing routes
 cartRouter.post("/add", Protect, addToCart);
 cartRouter.post("/remove", Protect, removeFromCart);
 cartRouter.get("/get", Protect, getCart);
+
+// 🆕 New route to update quantity
+cartRouter.put("/update-quantity", Protect, updateCartQuantity);
 
 export default cartRouter;
