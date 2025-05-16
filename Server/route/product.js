1bf4c07c950ab325ed9de90a5906c7b265a151e5
Firagos <jemalfiragos@gmail.com>
2025-05-16 07:17:32 +0300

    i returned to original

--- Server/route/product.js
+++ Server/route/product.js
@@ -5,15 +5,13 @@ import {
   getAllProduct,
 } from "../controller/productController.js";
 import { Protect, restrictTo } from "../controller/authController.js";
-import {upload}  from "../middleware/upload.js";
 
 const productRoute = express.Router();
 productRoute.get("/", getAllProduct);
 productRoute.post(
   "/create",
-  upload, // or use upload.fields(...) if you want stricter control
   createProduct
 );
-productRoute.delete("/:id", deleteProduct);
+productRoute.delete("/:id",Protect, deleteProduct);
 
 export default productRoute;
