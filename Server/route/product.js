198ae85904885d604da99ff49ec138d16ea2591c
Firagos <jemalfiragos@gmail.com>
2025-05-16 10:50:13 +0300

    i add upload function

--- Server/route/product.js
+++ Server/route/product.js
@@ -5,13 +5,15 @@ import {
   getAllProduct,
 } from "../controller/productController.js";
 import { Protect, restrictTo } from "../controller/authController.js";
+import { upload } from "../middleware/upload.js";
 
 const productRoute = express.Router();
 productRoute.get("/", getAllProduct);
 productRoute.post(
   "/create",
+  upload,           // ✅ This handles parsing form-data and populates req.body and req.files
   createProduct
 );
-productRoute.delete("/:id",Protect, deleteProduct);
+productRoute.delete("/:id", deleteProduct);
 
 export default productRoute;
