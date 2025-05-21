bbc394da9d85633f72d76cfe6fc74fb13f9aac49
Firagos <jemalfiragos@gmail.com>
2025-05-21 22:34:01 +0300

    adding the auth limiter to it

--- Server/route/user.js
+++ Server/route/user.js
@@ -1,8 +1,8 @@
 import express from "express";
 import { login, register } from "../controller/authController.js";
-
+import { authLimiter } from "../middleware/authLimit.js";
 const userRoute = express.Router();
-userRoute.post("/register", register);
-userRoute.post("/login", login);
+userRoute.post("/register", authLimiter, register);
+userRoute.post("/login", authLimiter, login);
 
 export default userRoute;
